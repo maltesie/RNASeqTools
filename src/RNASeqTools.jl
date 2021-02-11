62199e5669c02ab6e98b80a48a8b7a6a65b23928
Malte Siemers <malte.siemers@mailbox.org>
2021-02-11 11:13:41 +0100

    export local alignment

--- src/RNASeqTools.jl
+++ src/RNASeqTools.jl
@@ -1,6 +1,6 @@
 module RNASeqTools
 
-export align_mem, align_backtrack 
+export align_mem, align_backtrack, align_local
 export trim_fastp, split_libs 
 export read_bam, areconcordant, single_fragment_set
 export rilseq_analysis
